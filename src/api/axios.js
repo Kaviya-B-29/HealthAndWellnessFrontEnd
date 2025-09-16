a5565cc0213e1a5824d4bf800365c386b27826d6
Kaviya <kaviya@Kaviyas-MacBook-Air.local>
2025-09-16 20:41:52 +0530

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://healthandwellnessbackend-1.onrender.com/",
+  baseURL: "https://healthandwellnessbackend-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
